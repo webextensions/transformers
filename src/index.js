e34b1a5d4c08d51b4108b6e00bb3dc84e5ad0a47
Priyank Parashar <paras20xx@gmail.com>
2023-01-25 01:53:25 +0400

    Minor change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // eslint-disable-line node/file-extension-in-import
 
-
 import './index.css';
 
 import { App } from './App/App.js';
