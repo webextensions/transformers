9b117ed372d358d21e784a29962b8c13247d8e0f
Priyank Parashar <paras20xx@gmail.com>
2023-02-07 19:07:21 +0400

    Minor refactoring

--- src/App/PageFooter/PageFooter.js
+++ src/App/PageFooter/PageFooter.js
@@ -4,27 +4,21 @@ import styles from './PageFooter.css';
 
 import { CreatedByMessage } from 'generic-components/CreatedByMessage/CreatedByMessage.js';
 
-class PageFooter extends React.Component {
-    render() {
-        return (
-            <div className={styles.PageFooter}>
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        marginTop: 45,
-                        marginBottom: 20
-                    }}
-                >
-                    <div>
-                        <div>
-                            <CreatedByMessage />
-                        </div>
-                    </div>
-                </div>
+const PageFooter = function () {
+    return (
+        <div className={styles.PageFooter}>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: 45,
+                    marginBottom: 20
+                }}
+            >
+                <CreatedByMessage />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export { PageFooter };
